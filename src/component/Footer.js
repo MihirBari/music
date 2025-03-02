a600c3172317a5ace04792060dd8ca5b4f9389fe
MihirBari <barimihir23@gmail.com>
2025-03-02 17:15:56 +0530

    OK

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -2,22 +2,28 @@ import React from "react";
 import { FaYoutube } from "react-icons/fa";
 import { SiGmail } from "react-icons/si";
 import { FaPhone } from "react-icons/fa6";
+import { MdLocationPin } from "react-icons/md";
 
 export const Footer = () => {
   var currentYear = new Date().getFullYear();
 
   return (
     <footer style={footerStyle}>
-      <a href="https://www.youtube.com/channel/UCOtnrdNZOTYbbCh_HPEGptA" target="_blank" rel="noopener noreferrer" style={iconStyle}>
-        <FaYoutube />
-      </a>
-      <p>{`© ${currentYear} Musical Minds Band.`}</p>
-      <a href="mailto:gopaljighosal@gemail.com" style={iconStyle}>
-        <SiGmail />
-      </a>
-      <a href="tel:9833740995" target="_blank" rel="noopener noreferrer" style={iconStyle}>
-        <FaPhone />
-      </a>
+      <div style={contentStyle}>
+        <a href="https://www.youtube.com/channel/UCOtnrdNZOTYbbCh_HPEGptA" target="_blank" rel="noopener noreferrer" style={iconStyle}>
+          <FaYoutube />
+        </a>
+        <p>{`© ${currentYear} Musical Minds Band.`}</p>
+        <a href="mailto:gopaljighosal@gemail.com" style={iconStyle}>
+          <SiGmail />
+        </a>
+        <a href="tel:9833740995" target="_blank" rel="noopener noreferrer" style={iconStyle}>
+          <FaPhone />
+        </a>
+      </div>
+      <p style={{ textAlign: "center", display:"flex", flexDirection:"row" }}>
+      <MdLocationPin />     No. 50, Mittal Estate, 2nd Floor, Marol Naka, Andheri (East), Mumbai
+      </p>
     </footer>
   );
 };
@@ -26,15 +32,21 @@ const footerStyle = {
   backgroundColor: "#333",
   color: "#fff",
   display: "flex",
+  flexDirection: "column",
   justifyContent: "center",
   alignItems: "center",
-  gap: "15px",
-  padding: "10px",
+  padding: "15px",
+};
+
+const contentStyle = {
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "center",
+  gap: "10px",
 };
 
 const iconStyle = {
-  color: "#fff", // Ensure icons maintain color
+  color: "#fff",
   textDecoration: "none",
   fontSize: "1.5rem",
 };
-
